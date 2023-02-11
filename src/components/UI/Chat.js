8e38744121ba97691c1e617ce3bf28802c94d589
PathakTushar <tusharph1@gmail.com>
2023-02-11 20:11:20 +0530

    added chat module

--- src/components/UI/Chat.js
+++ src/components/UI/Chat.js
@@ -1,12 +1,38 @@
 import classes from "./Chat.module.css";
 import { BsFillChatTextFill } from "react-icons/bs";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
 const Chat = () => {
+  const [showChatWindow, setShowChatWindow] = useState(false);
+  const chatWindowHandler = () => {
+    setShowChatWindow(!showChatWindow);
+  };
   return (
-    <>
-      <button className={classes.chat}>
-        <BsFillChatTextFill /> Let's Chat!
-      </button>
-    </>
+    <div className={classes.chatOption}>
+      {showChatWindow && (
+        <div className={classes.chatWindow}>
+          <div className={classes.chatTitle}>
+            <div>
+              <h2>earthly</h2>
+              <p>We'll reply as soon as we can</p>
+            </div>
+            <button className={classes.closeButton} onClick={chatWindowHandler}>
+              <CloseIcon />
+            </button>
+          </div>
+          <input
+            type="text"
+            placeholder="Type your message..."
+            className={classes.chatInput}
+          />
+        </div>
+      )}
+      {!showChatWindow && (
+        <button className={classes.chat} onClick={chatWindowHandler}>
+          <BsFillChatTextFill /> Let's Chat!
+        </button>
+      )}
+    </div>
   );
 };
 export default Chat;
